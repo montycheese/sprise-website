446b5bba2c4640112ff7110f81de45d097c9eeaa
Kevin Boodtama <kboodtama@gmail.com>
2023-12-07 05:09:49 -0500

    Add OG and viewport meta tags

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,6 +5,35 @@ import Head from 'next/head'
 export const metadata: Metadata = {
   title: 'Sprise',
   description: 'A software development agency and product studio.',
+  openGraph: {
+    title: 'Sprise',
+    description: 'A software development agency and product studio.',
+    url: 'https://sprise.co',
+    images: [
+      {
+        url: '../static/img/sprise-logo-og.png',
+        width: '1200',
+        height: '675',
+        alt: 'Sprise',
+      }
+    ],
+    locale: 'en_US',
+    type: 'website',
+  },
+  twitter: {
+    card: 'summary_large_image',
+    title: 'Sprise',
+    description: 'A software development agency and product studio.',
+    creator: '@sprisehq',
+    images: ['../static/img/sprise-logo-og.png'],
+  },
+}
+
+export const viewport = {
+  themeColor: '#D2A7FC',
+  width: 'device-width',
+  initialScale: 1,
+  viewportFit: 'cover'
 }
 
 export default function RootLayout({
